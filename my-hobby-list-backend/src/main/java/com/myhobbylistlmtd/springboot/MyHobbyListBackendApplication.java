ea17298db321914dec9d9d3cce9026c228ae5228
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-08 14:02:20 -0300

    Initial config backend

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/MyHobbyListBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myhobbylistlmtd.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHobbyListBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHobbyListBackendApplication.class, args);
+	}
+
+}
---
+++ my-hobby-list-backend/src/test/java/com/myhobbylistlmtd/springboot/MyHobbyListBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhobbylistlmtd.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHobbyListBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
