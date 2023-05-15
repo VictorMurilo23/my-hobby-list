66d716d560679c35448b88ee81c6c00f2d622cf1
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-15 12:29:23 -0300

    Pacote request.body com package-info e RequestLoginBody documentado

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/request/body/RequestLoginBody.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/request/body/RequestLoginBody.java
@@ -1,22 +1,58 @@
 package com.myhobbylistlmtd.springboot.request.body;
 
+// TODO Usar essa classe quando for fazer o body de cadastro.
 public class RequestLoginBody {
-  public String email;
-  public String password;
+  /** Atributo email do body.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  private String email;
 
+  /** Atributo password do body.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  private String password;
+
+  /** Getter do atributo email.
+  * @return Retorna o email do body.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
   public String getEmail() {
     return email;
   }
 
-  public void setEmail(String email) {
-    this.email = email;
+  /** Setter do atributo email.
+  * @param userEmail Novo email do body
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  public void setEmail(final String userEmail) {
+    this.email = userEmail;
   }
 
+  /** Getter do atributo password.
+  * @return Retorna o password do body.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
   public String getPassword() {
     return password;
   }
 
-  public void setPassword(String password) {
-    this.password = password;
+  /** Setter do atributo password.
+  * @param userPassword Novo password do body
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  public void setPassword(final String userPassword) {
+    this.password = userPassword;
   }
 }
---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/request/body/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Pacote com todos os tipos de body que podem ser recebidos do front-end.
+*
+* @since 1.0
+* @author Victor Murilo
+* @version 1.0
+*/
+package com.myhobbylistlmtd.springboot.request.body;
