c384fa26506d7c20cd8ec1289917033dbf25c234
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-15 11:08:53 -0300

    Package info de User

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/User/package-info.java
@@ -0,0 +1,11 @@
+/**
+  * <p>
+  * Essa pacote contém o controller,
+  service, model e repository de tudo relacionado ao usuário.
+  * </p>
+  *
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+*/
+package com.myhobbylistlmtd.springboot.User;
