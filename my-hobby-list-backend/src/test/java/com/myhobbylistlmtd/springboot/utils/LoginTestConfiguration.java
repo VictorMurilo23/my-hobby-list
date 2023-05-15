1aa33e933a732c4a404bf2aaefbcaccd14758f7c
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-15 13:12:56 -0300

    LoginTestConfiguration agora herda de abstractmigration

--- my-hobby-list-backend/src/test/java/com/myhobbylistlmtd/springboot/utils/LoginTestConfiguration.java
+++ my-hobby-list-backend/src/test/java/com/myhobbylistlmtd/springboot/utils/LoginTestConfiguration.java
@@ -2,18 +2,17 @@ package com.myhobbylistlmtd.springboot.utils;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 
+import com.myhobbylistlmtd.springboot.abc.AbstractMigration;
 import com.myhobbylistlmtd.springboot.user.User;
 import com.myhobbylistlmtd.springboot.user.UserRepository;
 
 @TestConfiguration
-public class LoginTestConfiguration {
+public class LoginTestConfiguration extends AbstractMigration<UserRepository> {
 
-  @Bean
-  CommandLineRunner initDatabase(UserRepository repository) {
-    return args -> {
-      repository.save(new User("Victor", "email@gmail.com", "1456"));
-    };
+  @Override
+  public CommandLineRunner initDatabase(UserRepository repository) {
+    repository.save(new User("Victor", "email@gmail.com", "1456"));
+    return null;
   }
 }
