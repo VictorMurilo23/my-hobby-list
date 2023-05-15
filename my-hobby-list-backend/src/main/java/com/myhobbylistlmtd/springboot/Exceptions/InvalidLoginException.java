cab84b2ec0819ea262b8165c72e2531d8de1697f
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-15 11:06:57 -0300

    Documentação da exceção de login inválido

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/Exceptions/InvalidLoginException.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/Exceptions/InvalidLoginException.java
@@ -1,7 +1,14 @@
 package com.myhobbylistlmtd.springboot.Exceptions;
 
 public class InvalidLoginException extends RuntimeException {
-  public InvalidLoginException(String message){
+  /**.
+  * Recebe uma mensagem que irá ser exibida no response de erro
+  * @param message
+  * @since 1.0
+  * @version 1.0
+  * @author Victor Murilo
+  */
+  public InvalidLoginException(final String message) {
     super(message);
   }
 }
