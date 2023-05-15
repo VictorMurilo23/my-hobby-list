67fc427bb2c8f6cd323e50522c95783d8da3c4d5
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-15 13:50:59 -0300

    Typo nome da classe MediaStatus

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/status/MediaStatusRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/status/MediaStatusRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MediaTypeRepository extends JpaRepository<MediaStatus, Long> {
+public interface MediaStatusRepository extends JpaRepository<
+  MediaStatus, Long
+> {
 }
