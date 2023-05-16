1f97134b9816dea4df62437105400ed7609bf25b
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-16 10:39:22 -0300

    Application com importando as migrations

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/MyHobbyListBackendApplication.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/MyHobbyListBackendApplication.java
@@ -2,8 +2,13 @@ package com.myhobbylistlmtd.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.myhobbylistlmtd.springboot.migrations.LoadMediaStatus;
+import com.myhobbylistlmtd.springboot.migrations.LoadMediaTypes;
 
 @SpringBootApplication
+@Import({LoadMediaStatus.class, LoadMediaTypes.class})
 public class MyHobbyListBackendApplication {
 
 	public static void main(String[] args) {
