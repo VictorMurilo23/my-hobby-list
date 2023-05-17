fb7ed647190da004fe4435c6a20763257843a9c3
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-17 12:20:34 -0300

    Métodos de busca em MediaTypeRepository e MediaStatusRepository

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/type/MediaTypeRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/type/MediaTypeRepository.java
@@ -5,4 +5,13 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MediaTypeRepository extends JpaRepository<MediaType, Long> {
+  /**
+  * Faz uma busca por tipo.
+  * @param type Nome do tipo buscado.
+  * @return A classe MediaType ou null, caso não tenha sido encontrado o tipo.
+  * @since 1.0
+  * @version 1.0
+  * @author Victor Murilo
+  */
+  MediaType findByType(String type);
 }
