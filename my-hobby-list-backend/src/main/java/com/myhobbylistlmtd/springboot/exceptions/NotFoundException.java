cd037cff324edb0aa747fcbf5ecf8f30b68c2f9e
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-25 13:38:58 -0300

    Mudança no nome da exceção NotFoundImage para NotFound

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/exceptions/NotFoundException.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/exceptions/NotFoundException.java
@@ -4,15 +4,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ImageNotFoundException extends RuntimeException {
+public class NotFoundException extends RuntimeException {
   /**.
-  * Exceção lançada quando a imagem requisitada não é encontrada
+  * Exceção lançada quando algum elemento obrigatório
+  não é encontrado no banco de dados.
   * @param message
   * @since 1.0
   * @version 1.0
   * @author Victor Murilo
   */
-  public ImageNotFoundException(final String message) {
+  public NotFoundException(final String message) {
     super(message);
   }
 }
