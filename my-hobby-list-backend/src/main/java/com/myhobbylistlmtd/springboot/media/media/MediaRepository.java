963100d0d808a7bbb16a687d76151081ec43731c
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-30 13:16:52 -0300

    Método de achar 10 medias  mais recentes

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/media/MediaRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/media/MediaRepository.java
@@ -1,9 +1,18 @@
 package com.myhobbylistlmtd.springboot.media.media;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MediaRepository extends JpaRepository<Media, Long> {
-
+  /**
+  * Busca os últimos 10 medias inseridos.
+  * @return Os 10 medias recentemente inseridos no banco de dados.
+  * @since 1.0
+  * @version 1.0
+  * @author Victor Murilo
+  */
+  List<Media> findFirst10ByOrderByInsertionDateDesc();
 }
