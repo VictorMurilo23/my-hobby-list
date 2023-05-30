e17d89558faa567cdd6aea73b03c7a4ded98690f
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-30 12:45:32 -0300

    MediaParams e migration atualizadas com insertionDate

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/migrations/LoadMedia.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/migrations/LoadMedia.java
@@ -1,5 +1,6 @@
 package com.myhobbylistlmtd.springboot.migrations;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -89,19 +90,28 @@ public class LoadMedia {
                   .setVolumes(25)
                   .setStatus(statusRepo.findByStatus("Completo"))
                   .setType(typeRepo.findByType("Manga"))
-                  .setImageUrl("images/covers/gto-manga-capa.jpg")),
+                  .setImageUrl("images/covers/gto-manga-capa.jpg")
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:01:00")
+                  )),
           new Media(
               new MediaParams("Final Fantasy Tactics: The War of the Lions")
                   .setLength(42)
                   .setStatus(statusRepo.findByStatus("Completo"))
                   .setType(typeRepo.findByType("Jogo"))
-                  .setImageUrl("images/covers/fft-twotl-capa.jpg")),
+                  .setImageUrl("images/covers/fft-twotl-capa.jpg")
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:02:00")
+                  )),
           new Media(
               new MediaParams("Neon Genesis Evangelion: The End of Evangelion")
                   .setLength(1)
                   .setStatus(statusRepo.findByStatus("Completo"))
                   .setType(typeRepo.findByType("Filme"))
-                  .setImageUrl("images/covers/the-end-of-evangelion-capa.jpg")),
+                  .setImageUrl("images/covers/the-end-of-evangelion-capa.jpg")
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:03:00")
+                  )),
           new Media(
               new MediaParams("Gyakkyou Burai Kaiji: Ultimate Survivor")
                   .setLength(26)
@@ -109,6 +119,9 @@ public class LoadMedia {
                   .setType(typeRepo.findByType("Anime"))
                   .setImageUrl(
                     "images/covers/kaiji-1-temporada-anime-capa.jpg"
+                  )
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:04:00")
                   )),
           new Media(
               new MediaParams("Breaking Bad - 1° Temporada")
@@ -117,6 +130,9 @@ public class LoadMedia {
                   .setType(typeRepo.findByType("Série"))
                   .setImageUrl(
                     "images/covers/breaking-bad-1-temporada-capa.jpg"
+                  )
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:05:00")
                   )),
           new Media(
               new MediaParams("The Legendary Mechanic (Novel)")
@@ -126,6 +142,9 @@ public class LoadMedia {
                   .setType(typeRepo.findByType("Livro"))
                   .setImageUrl(
                     "images/covers/novel-the-legendary-mechanic-capa.jpg"
+                  )
+                  .setInsertionDate(
+                    LocalDateTime.parse("2023-05-30T09:06:00")
                   ))
       };
       List<Media> mediaList = new ArrayList<>();
