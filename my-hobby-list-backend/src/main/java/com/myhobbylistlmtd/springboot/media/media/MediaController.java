ce3d8b4f6804c7f4ad7009a4c6cfdba3366d71b4
Victor-Murilo <victor2706.vm@gmail.com>
2023-05-31 12:20:34 -0300

    Rota de media de procurar por nome

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/media/MediaController.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/media/MediaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class MediaController {
   /**
   * Rota de medias recém-adicionadas, não recebe nenhum parâmetro e
   só retorna uma lista de 10 medias.
-  * @return Retorna um token caso uma exceção não seja lançada.
+  * @return Retorna uma lista com 10 ou menos medias recém adicionadas.
   * @since 1.0
   * @version 1.0
   * @author Victor Murilo
@@ -32,6 +33,23 @@ public class MediaController {
   @GetMapping("/recent-add")
   ResponseMediasList findRecentMedias() {
     List<Media> recentMedias = mediaService.getMostRecentMedias();
+    ResponseMediasList response = new ResponseMediasList();
+    response.setMedias(recentMedias);
+    return response;
+  }
+
+  /**
+  * Rota de medias recém-adicionadas, não recebe nenhum parâmetro e
+  só retorna uma lista de 10 medias.
+  * @param mediaName O nome passado na url, utilizado na busca de medias.
+  * @return Retorna uma lista de objetos Media.
+  * @since 1.0
+  * @version 1.0
+  * @author Victor Murilo
+  */
+  @GetMapping("/search-by-name/{mediaName}")
+  ResponseMediasList findMediaByName(final @PathVariable String mediaName) {
+    List<Media> recentMedias = mediaService.findByName(mediaName);
     ResponseMediasList response = new ResponseMediasList();
     response.setMedias(recentMedias);
     return response;
