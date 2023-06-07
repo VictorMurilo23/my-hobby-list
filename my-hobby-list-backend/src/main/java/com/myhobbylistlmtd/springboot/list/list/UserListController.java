afaee77e39522d55a402602dbf4fed1410649e8b
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-07 14:24:28 -0300

    ResponseStatus de created pra rota insert

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListController.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListController.java
@@ -1,9 +1,11 @@
 package com.myhobbylistlmtd.springboot.list.list;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.myhobbylistlmtd.springboot.request.body.RequestUserListBody;
@@ -32,6 +34,7 @@ public class UserListController {
    * @author Victor Murilo
    */
   @PostMapping("/insert")
+  @ResponseStatus(HttpStatus.CREATED)
   ResponseMessage findRecentMedias(
     @RequestBody @Valid final RequestUserListBody body
   ) {
