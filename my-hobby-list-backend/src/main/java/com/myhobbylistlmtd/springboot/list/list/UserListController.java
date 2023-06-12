958a78b496f2f8798cf007f80fa758aa9339c709
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-12 15:28:52 -0300

    Método de retornar a lista do usuário em UserListController e response desse mesmo método

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListController.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListController.java
@@ -1,15 +1,21 @@
 package com.myhobbylistlmtd.springboot.list.list;
 
 import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myhobbylistlmtd.springboot.request.body.RequestUserId;
 import com.myhobbylistlmtd.springboot.request.body.RequestUserListBody;
 import com.myhobbylistlmtd.springboot.response.body.ResponseMessage;
+import com.myhobbylistlmtd.springboot.response.body.ResponseUserList;
 
 import jakarta.validation.Valid;
 
@@ -35,7 +41,7 @@ public class UserListController {
    */
   @PostMapping("/insert")
   @ResponseStatus(HttpStatus.CREATED)
-  ResponseMessage findRecentMedias(
+  ResponseMessage insertItemInUserlist(
     @RequestBody @Valid final RequestUserListBody body
   ) {
     String insertListItem = listService.insertItemInList(body);
@@ -44,4 +50,22 @@ public class UserListController {
     return response;
   }
 
+  /**
+   * Rota de pegar os itens da lista do usuário.
+   * @param body Um objeto de RequestUserId
+   * @return Um objeto contendo os itens da lista do usuário
+   * @since 1.0
+   * @version 1.0
+   * @author Victor Murilo
+   */
+  @GetMapping("/find")
+  @ResponseStatus(HttpStatus.OK)
+  ResponseUserList findUserlistItems(
+    @RequestBody @Valid final RequestUserId body
+  ) {
+    List<UserList> list = listService.findListByUserId(body.getUserId());
+    ResponseUserList response = new ResponseUserList();
+    response.setList(list);
+    return response;
+  }
 }
