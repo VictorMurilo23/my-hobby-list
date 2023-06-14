58e44e701ae8ee2ea496ec2bb12bbe84280a8a57
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-14 12:56:04 -0300

    Esqueleto da página de login e notfound

--- my-hobby-list-frontend/src/app/app.module.ts
+++ my-hobby-list-frontend/src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: "login", component: LoginComponent},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
