9a61c76469ec8b4bbed3fd389f0cf66a2c781557
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-15 12:55:00 -0300

    Arquivos de páginas na pasta de pages

--- my-hobby-list-frontend/src/app/app.module.ts
+++ my-hobby-list-frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { RouterModule } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     RouterModule.forRoot([
       {path: "login", component: LoginComponent},
       {path: '**', component: PageNotFoundComponent}
-    ])
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
