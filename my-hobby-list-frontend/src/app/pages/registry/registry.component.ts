b8e9b5e4645070a2847b96dd1f655b596f981b1b
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-16 11:47:20 -0300

    Página de registro completa

--- my-hobby-list-frontend/src/app/pages/registry/registry.component.ts
+++ my-hobby-list-frontend/src/app/pages/registry/registry.component.ts
@@ -1,6 +1,8 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import ErrorMessage from 'src/app/abstract/ErrorMessage';
 import IRegister from 'src/app/interfaces/IRegister';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -13,7 +15,32 @@ export class RegistryComponent extends ErrorMessage {
   private password: string = "";
   private username: string = ""; 
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private localStorage: LocalStorageService) {
     super("");
+  }
+
+  register() {
+    const body: IRegister = { email: this.email, password: this.password, username: this.username};
+    this.userService.register(body).subscribe({
+      next: (data) => {
+        this.localStorage.setToken(data.token);
+      },
+      error: (error: HttpErrorResponse) => super.setErrorMessage(error.error.message) 
+    });
+  }
+
+  saveEmail(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.email = input.value;
+  }
+
+  savePassword(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.password = input.value;
+  }
+
+  saveUsername(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.username = input.value;
   }
 }
