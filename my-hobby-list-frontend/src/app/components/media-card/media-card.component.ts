3185640d5268128fd2d6d55c7af08591d253122e
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-16 13:42:47 -0300

    Card de media redirecionando pra página da media clicada

--- my-hobby-list-frontend/src/app/components/media-card/media-card.component.ts
+++ my-hobby-list-frontend/src/app/components/media-card/media-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import IMedia from 'src/app/interfaces/IMedia';
 import { environment } from 'src/environments/environment.development';
 
@@ -10,11 +11,16 @@ import { environment } from 'src/environments/environment.development';
 export class MediaCardComponent implements OnInit {
   @Input() mediaName: string = "";
   @Input() imageUrl: string = "";
+  @Input() mediaId!: number;
   
-  constructor() {
+  constructor(private router: Router) {
   }
 
   ngOnInit(): void {
     this.imageUrl = `${environment.apiUrl}/${this.imageUrl}`
+  }
+
+  redirectToMediaPage() {
+    this.router.navigateByUrl(`/media/${this.mediaId}`)
   }
 }
