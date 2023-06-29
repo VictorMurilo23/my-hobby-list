66d0cebac1ae141abd01e1be776ce73237768e31
Victor-Murilo <victor2706.vm@gmail.com>
2023-06-29 15:36:31 -0300

    feat: página de insert pegando os status do back-end e colocando na tela

--- my-hobby-list-frontend/src/app/pages/insert/insert.component.ts
+++ my-hobby-list-frontend/src/app/pages/insert/insert.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { EMPTY, concatMap } from 'rxjs';
+import ErrorMessage from 'src/app/abstract/ErrorMessage';
+import IMedia from 'src/app/interfaces/IMedia';
+import { MediaService } from 'src/app/services/media.service';
+import { statusNameArray } from 'src/app/utils/statusNameArray';
 
 @Component({
   selector: 'app-insert',
   templateUrl: './insert.component.html',
   styleUrls: ['./insert.component.css']
 })
-export class InsertComponent {
+export class InsertComponent extends ErrorMessage implements OnInit {
+  public mediaInfo!: IMedia | null | undefined;
+  public insertStatusName: string[] = statusNameArray;
 
+  constructor(private mediaService: MediaService, private activatedRoute: ActivatedRoute) {
+    super("");
+  }
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap
+    .pipe(
+      concatMap((params: ParamMap) => {
+        const idParam = params.get("id");
+        if (idParam !== null) {
+          return this.mediaService.getMediaById(idParam);
+        }
+        return EMPTY;
+      })
+    )
+    .subscribe({
+      next: (data: IMedia) => {
+        this.mediaInfo = data;
+      },
+      error: () => {
+        this.mediaInfo = null;
+      }
+    });
+  }
 }
