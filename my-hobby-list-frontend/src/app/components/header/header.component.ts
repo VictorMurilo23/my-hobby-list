f9d875a624e1ef7f3abf064d0680a1fcfdb11bd8
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-07 15:54:29 -0300

    feat: header mostra se o usuário está logado ou não

--- my-hobby-list-frontend/src/app/components/header/header.component.ts
+++ my-hobby-list-frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Buffer } from 'buffer';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  public username! : string | null;
+  constructor(private localStorageService: LocalStorageService, private router: Router) {
+    this.router.events.subscribe((ev) => {
+      if (ev instanceof NavigationEnd) { 
+        this.parseJwt();
+      }
+    });
+  }
 
+  private parseJwt(): void {
+    try {
+      const token = this.localStorageService.getToken();
+      if (token === null) throw new Error();
+      const payload = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+      const { username } = payload;
+      if (username === null) throw new Error();
+      this.username = username;
+    } catch (e) {
+      this.localStorageService.removeToken();
+      this.username = null;
+    }
+  }
 }
