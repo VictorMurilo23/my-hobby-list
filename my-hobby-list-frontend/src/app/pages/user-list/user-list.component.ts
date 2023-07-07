6eea900606bedfa36bee084055ffa58fa0c9cb0a
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-07 13:38:39 -0300

    feat: página user-list pegando e renderização a lista do usuário

--- my-hobby-list-frontend/src/app/pages/user-list/user-list.component.ts
+++ my-hobby-list-frontend/src/app/pages/user-list/user-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { EMPTY, concatMap } from 'rxjs';
+import IUserListBody from 'src/app/interfaces/IUserListBody';
+import { ListService } from 'src/app/services/list.service';
+import UserListObj from 'src/app/types/UserListObj';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+  public userList!: UserListObj[] | null;
 
+  constructor(private listService: ListService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap
+    .pipe(
+      concatMap((params: ParamMap) => {
+        const usernameParam = params.get("username");
+        if (usernameParam !== null) {
+          return this.listService.findList(usernameParam);
+        }
+        return EMPTY;
+      })
+    )
+    .subscribe({
+      next: (data: IUserListBody) => {
+        this.userList = data.list;
+      },
+      error: () => {
+        this.userList = null;
+      }
+    });
+  }
 }
