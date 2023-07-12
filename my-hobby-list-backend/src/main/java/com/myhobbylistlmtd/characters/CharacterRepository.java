c7e271e4aad8e97a54133fb23328b37db43c0a40
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-12 15:10:49 -0300

    feat: repository de Character

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/characters/CharacterRepository.java
@@ -0,0 +1,8 @@
+package com.myhobbylistlmtd.characters;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharacterRepository extends JpaRepository<Characters, Long> {
+}
