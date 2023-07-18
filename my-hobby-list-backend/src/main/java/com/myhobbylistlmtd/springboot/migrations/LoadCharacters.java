b62550489f718747ce2a9b4ca4cd03e40525b972
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-18 13:23:25 -0300

    feat: migration de Characters

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/migrations/LoadCharacters.java
@@ -0,0 +1,30 @@
+package com.myhobbylistlmtd.springboot.migrations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.myhobbylistlmtd.springboot.characters.CharacterRepository;
+import com.myhobbylistlmtd.springboot.characters.Characters;
+
+@Configuration
+@Profile({ "production" })
+public class LoadCharacters {
+  /**
+   * Salvar os tipos de status que itens da lista podem ter.
+   * @param repository Repositório de ItemStatus
+   * @return null
+   */
+  @Bean
+  public CommandLineRunner runLoadCharacters(
+      final CharacterRepository repository) {
+    List<Characters> statusList = new ArrayList<Characters>();
+    statusList.add(new Characters("Onizuka", "Professor"));
+    repository.saveAll(statusList);
+    return null;
+  }
+}
