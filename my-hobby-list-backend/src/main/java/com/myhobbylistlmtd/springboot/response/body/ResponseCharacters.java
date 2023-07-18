4780af183173f2d1beb407c898568ca47b5f29ea
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-18 13:19:57 -0300

    feat: response da rota de procurar os personagens

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/response/body/ResponseCharacters.java
@@ -0,0 +1,40 @@
+package com.myhobbylistlmtd.springboot.response.body;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.myhobbylistlmtd.springboot.media.characters.MediaCharacters;
+import com.myhobbylistlmtd.springboot.utils.Views;
+
+public class ResponseCharacters {
+  /**
+   * Lista contendo os personagens referentes a uma media.
+   * @since 1.0
+   * @author Victor Murilo
+   * @version 1.0
+   */
+  @JsonView(Views.Public.class)
+  private List<MediaCharacters> characters;
+
+  /**
+   * Getter de characters.
+   * @return Uma lista contendo os personagens
+   * @since 1.0
+   * @author Victor Murilo
+   * @version 1.0
+   */
+  public List<MediaCharacters> getCharacters() {
+    return characters;
+  }
+
+  /**
+   * Setter de characters.
+   * @param charactersList Lista com personagens
+   * @since 1.0
+   * @author Victor Murilo
+   * @version 1.0
+   */
+  public void setCharacters(final List<MediaCharacters> charactersList) {
+    this.characters = charactersList;
+  }
+}
