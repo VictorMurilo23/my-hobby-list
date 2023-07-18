c4dfbf6571bffa3dc4a219690c186c074d3e8cb0
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-18 13:23:12 -0300

    feat: migration de CharactersRoles

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/migrations/LoadCharacterRoles.java
@@ -0,0 +1,33 @@
+package com.myhobbylistlmtd.springboot.migrations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.myhobbylistlmtd.springboot.characters.role.CharactersRole;
+import com.myhobbylistlmtd.springboot.characters.role.CharactersRoleRepository;
+
+@Configuration
+@Profile({ "production" })
+public class LoadCharacterRoles {
+  /**
+   * Salvar os tipos de status que itens da lista podem ter.
+   * @param repository Repositório de ItemStatus
+   * @return null
+   */
+  @Bean
+  public CommandLineRunner runLoadCharacterRoles(
+      final CharactersRoleRepository repository) {
+    List<CharactersRole> statusList = new ArrayList<CharactersRole>();
+    statusList.add(new CharactersRole("Personagem principal"));
+    statusList.add(new CharactersRole("Personagem secundário"));
+    statusList.add(new CharactersRole("Antagonista"));
+
+    repository.saveAll(statusList);
+    return null;
+  }
+}
