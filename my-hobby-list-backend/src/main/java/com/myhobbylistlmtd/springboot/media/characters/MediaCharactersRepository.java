77045d5853a7828086184741d7c92dcdafaabada
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-18 13:22:02 -0300

    feat: repository com o método de procurar personagens pelo mediaId

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/characters/MediaCharactersRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/media/characters/MediaCharactersRepository.java
@@ -1,9 +1,21 @@
 package com.myhobbylistlmtd.springboot.media.characters;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MediaCharactersRepository
 extends JpaRepository<MediaCharacters, MediaCharactersId> {
+  /**
+   * Procura todos os personagens de uma media.
+   * @param mediaId Id da media.
+   * @return Uma lista com todos os personagens daquela media
+   * @since 1.0
+   * @author Victor Murilo
+   * @version 1.0
+   */
+  // CHECKSTYLE:OFF: MethodNameCheck
+  List<MediaCharacters> findAllById_MediaId(Long mediaId);
 }
