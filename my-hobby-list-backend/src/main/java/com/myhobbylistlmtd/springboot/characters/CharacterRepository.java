8388cca00b73982621baf8cd3d39fb4299139f8e
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-19 11:45:11 -0300

    feat: CharacterRepository com o método de buscar personagem por nome

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/characters/CharacterRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/characters/CharacterRepository.java
@@ -5,4 +5,10 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CharacterRepository extends JpaRepository<Characters, Long> {
+  /**
+   * Procura um personagem por nome.
+   * @param characterName Nome do personagem
+   * @return Retorna um objeto Characters com as informações do personagem
+   */
+  Characters findByName(String characterName);
 }
