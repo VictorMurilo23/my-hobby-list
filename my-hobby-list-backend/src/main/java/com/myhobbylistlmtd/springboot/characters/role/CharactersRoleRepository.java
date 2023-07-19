3ddab7eeedf6be5eca9335731db508f5a9aac948
Victor-Murilo <victor2706.vm@gmail.com>
2023-07-19 11:46:34 -0300

    feat: CharactersRoleRepository com o método de procurar por nome do papel

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/characters/role/CharactersRoleRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/characters/role/CharactersRoleRepository.java
@@ -4,4 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CharactersRoleRepository
     extends JpaRepository<CharactersRole, Long> {
+  /**
+   * Busca o papel que pode ser executado por nome.
+   * @param roleName Nome do papel
+   * @return Um objeto com o nome e id do papel
+   */
+  CharactersRole findByRoleName(String roleName);
 }
