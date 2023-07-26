21cb622d6822da2b206f4a40684807bd3a19281a
Victor Murilo <victor2706.vm@gmail.com>
2023-07-26 12:29:50 -0300

    test: testes de character.service parando de falhar

--- my-hobby-list-frontend/src/app/services/character.service.spec.ts
+++ my-hobby-list-frontend/src/app/services/character.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CharacterService } from './character.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('CharacterService', () => {
   let service: CharacterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [CharacterService]
+    });
     service = TestBed.inject(CharacterService);
   });
 
