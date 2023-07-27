9282a4e19c988b1d8181edbc20e5e2b2aa124955
Victor Murilo <victor2706.vm@gmail.com>
2023-07-27 13:20:43 -0300

    feat: Método no repository de UserList utilizado pra fazer buscar com o nome do usuário e status

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/list/list/UserListRepository.java
@@ -21,4 +21,17 @@ UserList, UserListId
    */
   // CHECKSTYLE:OFF: MethodNameCheck
   List<UserList> findAllById_UserId(User user);
+
+  /**
+   * Encontra os itens da lista do usuário com base no username e statusName.
+   * @param username Nome do usuário utilizado na busca
+   * @param statusName Nome do status utilizado na busca
+   * @return Uma lista contendo os itens de userlist
+   * @since 1.0
+   * @version 1.0
+   * @author Victor Murilo
+   */
+  List<UserList> findAllById_UserId_UsernameAndStatus_StatusName(
+    String username, String statusName
+  );
 }
