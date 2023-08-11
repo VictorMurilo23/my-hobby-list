7accdf9a4aef503fc7f0ab1a055f1f96925a8506
Victor Murilo <victor2706.vm@gmail.com>
2023-08-11 12:33:53 -0300

    feat: tabela reviews sem os relacionamentos

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviews/Reviews.java
@@ -0,0 +1,37 @@
+package com.myhobbylistlmtd.springboot.reviews;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "reviews")
+public class Reviews {
+  /** Id gerado automaticamente.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  /** Conteúdo da review.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  @Column(length = 10000, name = "content", nullable = false)
+  private String content;
+
+  /** Campo indicando se a review foi editada ou não.
+  * @since 1.0
+  * @author Victor Murilo
+  * @version 1.0
+  */
+  @Column(name = "edited", nullable = false)
+  private Boolean edited = false;
+}
