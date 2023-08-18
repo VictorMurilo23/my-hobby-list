c9586e027e27d0eb0c0259d385184bb8d598b4a1
Victor Murilo <victor2706.vm@gmail.com>
2023-08-18 12:49:15 -0300

    feat: Controller de reviews com rota de criar uma review

---
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviews/ReviewsController.java
@@ -0,0 +1,44 @@
+package com.myhobbylistlmtd.springboot.reviews;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myhobbylistlmtd.springboot.request.body.RequestCreateReview;
+import com.myhobbylistlmtd.springboot.response.body.ResponseMessage;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/reviews")
+public class ReviewsController {
+  /**
+   * Service de reviews.
+   */
+  @Autowired
+  private ReviewsService reviewsService;
+
+  /**
+   * blabla.
+   * @param body
+   * @param userId
+   * @return f.
+   */
+  @PostMapping("/create")
+  @ResponseStatus(HttpStatus.OK)
+  ResponseMessage createReview(
+    @Valid @RequestBody final RequestCreateReview body,
+    @RequestAttribute("userId") final Long userId
+  ) {
+    reviewsService.createReview(body, userId);
+    ResponseMessage response = new ResponseMessage();
+    response.setMessage("Review criado com sucesso!");
+    System.out.print(userId);
+    return response;
+  }
+}
