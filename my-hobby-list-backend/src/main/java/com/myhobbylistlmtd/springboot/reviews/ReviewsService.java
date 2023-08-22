1e594509d5ead68b9bd05224ae898a3d5a2b0aec
Victor Murilo <victor2706.vm@gmail.com>
2023-08-22 12:50:24 -0300

    feat: método de editar review em ReviewsService

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviews/ReviewsService.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviews/ReviewsService.java
@@ -3,9 +3,11 @@ package com.myhobbylistlmtd.springboot.reviews;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myhobbylistlmtd.springboot.exceptions.NotFoundException;
 import com.myhobbylistlmtd.springboot.media.media.Media;
 import com.myhobbylistlmtd.springboot.media.media.MediaService;
 import com.myhobbylistlmtd.springboot.request.body.RequestCreateReview;
+import com.myhobbylistlmtd.springboot.request.body.RequestEditReview;
 import com.myhobbylistlmtd.springboot.user.User;
 import com.myhobbylistlmtd.springboot.user.UserService;
 
@@ -30,6 +32,20 @@ public class ReviewsService {
   private MediaService mediaService;
 
   /**
+   * d.
+   * @param userId d
+   * @param mediaId d
+   * @return d
+   */
+  private Reviews findReview(final Long userId, final Long mediaId) {
+    Reviews review = reviewsRepo.findReviewsByUserIdAndMediaId(userId, mediaId);
+    if (review == null) {
+      throw new NotFoundException("Review não encontrada!");
+    }
+    return review;
+  }
+
+  /**
    * bla.
    * @param body
    * @param userId
@@ -40,6 +56,23 @@ public class ReviewsService {
     Reviews review = new Reviews(
       body.getContent(), body.getRecommended(), media, user
     );
+    reviewsRepo.save(review);
+  }
+
+  /**
+   * d.
+   * @param body f
+   * @param userId f
+   */
+  public void editReview(final RequestEditReview body, final Long userId) {
+    Reviews review = this.findReview(userId, body.getMediaId());
+    if (body.getContent() != null) {
+      review.setContent(body.getContent());
+    }
+    if (body.getRecommended() != null) {
+      review.setRecommended(body.getRecommended());
+    }
+    review.setEdited(true);
     reviewsRepo.save(review);
   }
 }
