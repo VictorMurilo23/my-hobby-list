20f3ccd074c857e43bf65ed260f0c9f34d4ed8ec
Victor Murilo <victor2706.vm@gmail.com>
2023-08-22 18:05:55 -0300

    feat: TestConfiguration e Profile em EditReviewsConfiguration que eu tinha esquecido de colocar

--- my-hobby-list-backend/src/test/java/com/myhobbylistlmtd/springboot/utils/EditReviewsConfiguration.java
+++ my-hobby-list-backend/src/test/java/com/myhobbylistlmtd/springboot/utils/EditReviewsConfiguration.java
@@ -3,7 +3,9 @@ package com.myhobbylistlmtd.springboot.utils;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.myhobbylistlmtd.springboot.media.media.Media;
 import com.myhobbylistlmtd.springboot.media.media.MediaRepository;
@@ -17,6 +19,8 @@ import com.myhobbylistlmtd.springboot.reviews.ReviewsRepository;
 import com.myhobbylistlmtd.springboot.user.User;
 import com.myhobbylistlmtd.springboot.user.UserRepository;
 
+@TestConfiguration
+@Profile({ "test" })
 public class EditReviewsConfiguration {
   @Bean
   public CommandLineRunner runEditReviewsConfiguration(UserRepository userRepo, MediaRepository mediaRepo,
