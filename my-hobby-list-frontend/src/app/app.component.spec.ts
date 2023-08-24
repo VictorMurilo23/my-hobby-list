afd2256291d79fa5745ba181b1ff062727c810d0
Victor Murilo <victor2706.vm@gmail.com>
2023-08-24 15:35:56 -0300

    test: HttpClientModule nos testes de app.component

--- my-hobby-list-frontend/src/app/app.component.spec.ts
+++ my-hobby-list-frontend/src/app/app.component.spec.ts
@@ -3,12 +3,14 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
         AppComponent, HeaderComponent, SearchBarComponent
