387dcdea07c9353d0cf6920cb129e3789c07da25
Victor Murilo <victor2706.vm@gmail.com>
2023-08-24 12:18:48 -0300

    feat: change-profile-image renderizando as possíveis imagens de perfil

--- my-hobby-list-frontend/src/app/components/change-profile-image/change-profile-image.component.ts
+++ my-hobby-list-frontend/src/app/components/change-profile-image/change-profile-image.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-change-profile-image',
   templateUrl: './change-profile-image.component.html',
   styleUrls: ['./change-profile-image.component.css']
 })
-export class ChangeProfileImageComponent {
+export class ChangeProfileImageComponent implements OnInit {
+  private imagesUrls: string[] = [];
+  private errorMessage!: string;
+  readonly env = environment;
 
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getAllProfileImagesOptions().subscribe({
+      next: (data) => {
+        this.imagesUrls = data.images;
+      },
+      error: () => {
+        this.errorMessage = "Ops! Algo deu errado";
+      }
+    })
+  }
+
+  public getImages() {
+    return this.imagesUrls;
+  }
+
+  public getErrorMessage() {
+    return this.errorMessage;
+  }
 }
