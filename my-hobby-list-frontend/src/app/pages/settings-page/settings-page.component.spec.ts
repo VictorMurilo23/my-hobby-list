fd01e15121fa3beb9cb61005adfe4db2af772179
Victor Murilo <victor2706.vm@gmail.com>
2023-08-24 14:56:04 -0300

    test: testes settings-page

--- my-hobby-list-frontend/src/app/pages/settings-page/settings-page.component.spec.ts
+++ my-hobby-list-frontend/src/app/pages/settings-page/settings-page.component.spec.ts
@@ -1,18 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
 import { SettingsPageComponent } from './settings-page.component';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+import routes from 'src/app/app.routes';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { By } from '@angular/platform-browser';
+import { ChangeProfileImageComponent } from 'src/app/components/change-profile-image/change-profile-image.component';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { LoginComponent } from '../login/login.component';
 
 describe('SettingsPageComponent', () => {
   let component: SettingsPageComponent;
   let fixture: ComponentFixture<SettingsPageComponent>;
+  let userService: UserService;
+  let router: Router;
+  let localStorage: LocalStorageService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SettingsPageComponent ]
+      imports: [HttpClientModule, RouterTestingModule.withRoutes(routes)],
+      declarations: [ SettingsPageComponent, ChangeProfileImageComponent, LoginComponent ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(SettingsPageComponent);
+    userService = fixture.debugElement.injector.get(UserService);
+    localStorage = fixture.debugElement.injector.get(LocalStorageService);
+    router = TestBed.inject(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -20,4 +36,21 @@ describe('SettingsPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect to login when localStorage token is null', fakeAsync(() => {
+    const { debugElement } = fixture;
+    spyOn(localStorage, 'getToken').and.returnValue(null);
+    fixture.detectChanges();
+
+    expect(router.url).toBe("/login")
+  }));
+
+  it('should redirect to change-profile-image on click', fakeAsync(() => {
+    const { debugElement } = fixture;
+    const changeProfileImageLink = debugElement.query(By.css(".change-profile-image-link"));
+    changeProfileImageLink.nativeElement.click();
+    tick();
+    fixture.detectChanges();
+    expect(router.url).toBe("/change-profile-image");
+  }));
 });
