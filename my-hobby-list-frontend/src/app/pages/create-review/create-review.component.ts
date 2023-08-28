5d5ac76ad5a2aae16d73117e5284540491e7388b
Victor Murilo <victor2706.vm@gmail.com>
2023-08-28 14:50:58 -0300

    feat: componente create-review com método de mandar a review e salvar as informações escritas

--- my-hobby-list-frontend/src/app/pages/create-review/create-review.component.ts
+++ my-hobby-list-frontend/src/app/pages/create-review/create-review.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CreateReview } from 'src/app/interfaces/IReviews';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { ReviewService } from 'src/app/services/review.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-create-review',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-review.component.css']
 })
 export class CreateReviewComponent {
+  @Input() mediaId!: number;
+  private review: CreateReview;
 
+  constructor(private reviewService: ReviewService, private localStorage: LocalStorageService, private userService: UserService) {
+    this.review = { content: "", mediaId: this.mediaId, recommended: true };
+  }
+
+  public setReviewContent(content: string) {
+    this.review.content = content;
+  }
+
+  public setReviewRecommended(recommended: boolean) {
+    this.review.recommended = recommended;
+  }
+
+  public getReviewRecommended() {
+    return this.review.recommended;
+  }
+
+  public getReviewContent() {
+    return this.review.content;
+  }
+
+  public sendReview() {
+    const token = this.localStorage.getToken();
+    if (token === null) {
+      this.userService.logout();
+      return;
+    }
+    this.reviewService.createReview(this.review, token);
+  }
 }
