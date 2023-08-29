6a95fbcec43e1e0328199ac350784b4556ae5a98
Victor Murilo <victor2706.vm@gmail.com>
2023-08-29 14:19:53 -0300

    feat: review do usuário, caso o mesmo tenha feito uma, aparece na página de reviews

--- my-hobby-list-frontend/src/app/components/reviews/reviews.component.ts
+++ my-hobby-list-frontend/src/app/components/reviews/reviews.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Review } from 'src/app/interfaces/IReviews';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
@@ -14,10 +15,13 @@ export class ReviewsComponent implements OnInit {
   public loading = true;
   private currentPage = 0;
   private totalPages!: number;
+  private userReview!: Review;
+  public showCreateReviewComponent = true;
 
   constructor(
     private reviewService: ReviewService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private localStorage: LocalStorageService
   ) {
     this.route.parent?.paramMap.subscribe(({ params: { id } }: any) => {
       this.mediaId = id;
@@ -35,8 +39,25 @@ export class ReviewsComponent implements OnInit {
     this.loading = false;
   }
 
+  private findUserReview() {
+    const token = this.localStorage.getToken();
+    if (token === null) {
+      return;
+    }
+    this.reviewService.findUserReview(this.mediaId, token).subscribe({
+      next: (review) => {
+        this.userReview = review;
+        this.showCreateReviewComponent = false;
+      },
+      error: (_err) => {
+        this.showCreateReviewComponent = true;
+      }
+    });
+  }
+
   ngOnInit(): void {
     this.findReviews();
+    this.findUserReview();
   }
 
   public getPages() {
@@ -61,5 +82,9 @@ export class ReviewsComponent implements OnInit {
 
   public getMediaId() {
     return this.mediaId;
+  }
+
+  public getUserReview() {
+    return this.userReview;
   }
 }
