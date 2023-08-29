1846a7ab6082eeea5d29eb8945d0303ab370fe5d
Victor Murilo <victor2706.vm@gmail.com>
2023-08-29 14:36:49 -0300

    feat: componente da página de reviews de um usuário

---
+++ my-hobby-list-frontend/src/app/pages/user-reviews-page/user-reviews-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserReviewsPageComponent } from './user-reviews-page.component';
+
+describe('UserReviewsPageComponent', () => {
+  let component: UserReviewsPageComponent;
+  let fixture: ComponentFixture<UserReviewsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserReviewsPageComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(UserReviewsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ my-hobby-list-frontend/src/app/pages/user-reviews-page/user-reviews-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-reviews-page',
+  templateUrl: './user-reviews-page.component.html',
+  styleUrls: ['./user-reviews-page.component.css']
+})
+export class UserReviewsPageComponent {
+
+}
