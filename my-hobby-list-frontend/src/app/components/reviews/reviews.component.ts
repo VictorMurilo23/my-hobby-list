14a2a94bfda096da303c071b0f700d5214b847ab
Victor Murilo <victor2706.vm@gmail.com>
2023-08-31 17:48:38 -0300

    refactor: ReviewsComponent refatorado pra se adequar ao novo CreateReviewComponent

--- my-hobby-list-frontend/src/app/components/reviews/reviews.component.ts
+++ my-hobby-list-frontend/src/app/components/reviews/reviews.component.ts
@@ -1,27 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Review } from 'src/app/interfaces/IReviews';
+import { CreateReview, Review } from 'src/app/interfaces/IReviews';
+import SendReview from 'src/app/interfaces/SendReview';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { ReviewService } from 'src/app/services/review.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css'],
 })
-export class ReviewsComponent implements OnInit {
+export class ReviewsComponent implements OnInit, SendReview {
   private reviews: Review[] = [];
   private mediaId!: number;
   public loading = true;
   private currentPage = 0;
   private totalPages!: number;
-  private userReview!: Review;
+  private userReview: Review = { content: "", recommended: true, user: { username: "" } };
   public showCreateReviewComponent = true;
 
   constructor(
     private reviewService: ReviewService,
     private route: ActivatedRoute,
-    private localStorage: LocalStorageService
+    private localStorage: LocalStorageService,
+    private userService: UserService
   ) {
     this.route.parent?.paramMap.subscribe(({ params: { id } }: any) => {
       this.mediaId = id;
@@ -87,4 +90,13 @@ export class ReviewsComponent implements OnInit {
   public getUserReview() {
     return this.userReview;
   }
+
+  sendReview = (review: CreateReview): void => {
+    const token = this.localStorage.getToken();
+    if (token === null) {
+      this.userService.logout();
+      return;
+    }
+    this.reviewService.createReview(review, token).subscribe();
+  };
 }
