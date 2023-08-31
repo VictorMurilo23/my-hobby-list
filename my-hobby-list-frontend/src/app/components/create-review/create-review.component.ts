d772d8727b4caeac1a928eeff057a47dc147d638
Victor Murilo <victor2706.vm@gmail.com>
2023-08-31 16:48:23 -0300

    refactor: CreateReviewComponent refatorado pra poder ser usado pra editar uma review

--- my-hobby-list-frontend/src/app/components/create-review/create-review.component.ts
+++ my-hobby-list-frontend/src/app/components/create-review/create-review.component.ts
@@ -1,45 +1,35 @@
 import { Component, Input } from '@angular/core';
-import { CreateReview } from 'src/app/interfaces/IReviews';
-import { LocalStorageService } from 'src/app/services/local-storage.service';
-import { ReviewService } from 'src/app/services/review.service';
-import { UserService } from 'src/app/services/user.service';
+import { CreateReview, Review } from 'src/app/interfaces/IReviews';
 
 @Component({
-  selector: 'app-create-review',
+  selector: 'app-create-review[sendReview][mediaId][reviewInfo]',
   templateUrl: './create-review.component.html',
   styleUrls: ['./create-review.component.css']
 })
 export class CreateReviewComponent {
+  @Input() reviewInfo!: Review;
   @Input() mediaId!: number;
-  private review: CreateReview;
-
-  constructor(private reviewService: ReviewService, private localStorage: LocalStorageService, private userService: UserService) {
-    this.review = { content: "", mediaId: this.mediaId, recommended: true };
-  }
+  @Input()
+  sendReview!: (review: CreateReview) => void; 
 
   public setReviewContent(content: string) {
-    this.review.content = content;
+    this.reviewInfo.content = content;
   }
 
   public setReviewRecommended(recommended: boolean) {
-    this.review.recommended = recommended;
+    this.reviewInfo.recommended = recommended;
   }
 
   public getReviewRecommended() {
-    return this.review.recommended;
+    return this.reviewInfo.recommended;
   }
 
   public getReviewContent() {
-    return this.review.content;
+    return this.reviewInfo.content;
   }
-
-  public sendReview() {
-    const token = this.localStorage.getToken();
-    if (token === null) {
-      this.userService.logout();
-      return;
-    }
-    this.review.mediaId = this.mediaId;
-    this.reviewService.createReview(this.review, token).subscribe();
+  
+  public send() {
+    const { content, recommended } = this.reviewInfo;
+    this.sendReview({ content, recommended, mediaId: this.mediaId });
   }
 }
