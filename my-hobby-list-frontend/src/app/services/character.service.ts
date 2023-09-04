2a05563d11dfe20c40b4cddef21f8b7af952ab79
Victor Murilo <victor2706.vm@gmail.com>
2023-09-04 15:47:28 -0300

    docs: character service

--- my-hobby-list-frontend/src/app/services/character.service.ts
+++ my-hobby-list-frontend/src/app/services/character.service.ts
@@ -12,6 +12,11 @@ export class CharacterService {
 
   constructor(private http: HttpClient) { }
 
+  /**
+   * Busca os personagens de uma media específica
+   * @param mediaName Nome da media
+   * @returns Em caso de sucesso o observável retorna um objeto contendo um array com nome de personagem e papel executado pelo mesmo, caso ocorra algum erro, retorna um objeto com uma chave message dizendo que erro aconteceu
+   */
   public getCharacters(mediaName: string): Observable<ICharacters> {
     return this.http.get<ICharacters>(`${this.baseUrl}/${mediaName}`, {
       observe: 'body',
