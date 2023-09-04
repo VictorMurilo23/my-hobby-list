9e7b4ba33c68ce3fb2bb5879028420929e5e0ec9
Victor Murilo <victor2706.vm@gmail.com>
2023-09-04 15:49:11 -0300

    docs: localStorage service

--- my-hobby-list-frontend/src/app/services/local-storage.service.ts
+++ my-hobby-list-frontend/src/app/services/local-storage.service.ts
@@ -10,14 +10,25 @@ export class LocalStorageService {
     this.storage = window.localStorage;
   }
 
+  /**
+   * Coloca o token no localStorage
+   * @param token Token jwt
+   */
   setToken(token: string) {
     this.storage.setItem("token", token)
   }
 
+  /**
+   * Remove o token do localStorage
+   */
   removeToken(): void {
     this.storage.removeItem("token");
   }
 
+  /**
+   * Pega o token do localStorage
+   * @returns Retorna o token salvo no localStorage ou null
+   */
   getToken(): string | null {
     return this.storage.getItem("token")
   }
