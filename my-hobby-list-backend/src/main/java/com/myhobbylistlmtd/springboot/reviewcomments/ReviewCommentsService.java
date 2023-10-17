ac16cb354f968081b6b5395feb4b965ce52639a6
Victor Murilo <victor2706.vm@gmail.com>
2023-10-17 12:21:26 -0300

    feat: Service de ReviewComments com método de criar um comentário

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsService.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsService.java
@@ -3,6 +3,12 @@ package com.myhobbylistlmtd.springboot.reviewcomments;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.myhobbylistlmtd.springboot.request.body.RequestCreateReviewComment;
+import com.myhobbylistlmtd.springboot.reviews.Reviews;
+import com.myhobbylistlmtd.springboot.reviews.ReviewsService;
+import com.myhobbylistlmtd.springboot.user.User;
+import com.myhobbylistlmtd.springboot.user.UserService;
+
 @Service
 public class ReviewCommentsService {
   /**
@@ -12,6 +18,18 @@ public class ReviewCommentsService {
   private ReviewCommentsRepository reviewCommentsRepo;
 
   /**
+   * Service de Reviews.
+   */
+  @Autowired
+  private ReviewsService reviewsService;
+
+  /**
+   * Service de User.
+   */
+  @Autowired
+  private UserService userService;
+
+  /**
    * Constructor utilizado nos testes unitários.
    * @param reviewCommentsRepo Mock de ReviewCommentsRepository
    */
@@ -19,5 +37,27 @@ public class ReviewCommentsService {
     final ReviewCommentsRepository reviewCommentsRepo
   ) {
     this.reviewCommentsRepo = reviewCommentsRepo;
+  }
+
+  /**
+   * Cria um comentário em uma review.
+   * @param userId Id do usuário que está fazendo o comentário
+   * @param body Corpo da requisição com mediaId,
+   * nome do usuário que fez a review e conteúdo do comentário.
+   * @return Retorna um objeto contendo as informações do comentário.
+   */
+  public ReviewComments createReview(
+    final Long userId, final RequestCreateReviewComment body
+  ) {
+    Reviews review = reviewsService.findReviewByUsernameAndMediaId(
+      body.getUsernameFromReview(), body.getMediaId()
+    );
+    User user = userService.findById(userId);
+    ReviewComments comment = new ReviewComments(
+      review,
+      user,
+      body.getCommentary()
+    );
+    return reviewCommentsRepo.save(comment);
   }
 }
