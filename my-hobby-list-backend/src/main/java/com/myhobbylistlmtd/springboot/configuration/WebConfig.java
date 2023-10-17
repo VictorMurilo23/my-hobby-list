7925b46c1109c0bd95ea57a20e741756c6493eb3
Victor Murilo <victor2706.vm@gmail.com>
2023-10-17 12:18:08 -0300

    feat: endpoint /review-comments/create com token interceptor

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/configuration/WebConfig.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/configuration/WebConfig.java
@@ -19,6 +19,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(tokenInterceptor).addPathPatterns("/list/insert");
     registry.addInterceptor(tokenInterceptor).addPathPatterns("/user/profile/change-profile-image");
+    registry.addInterceptor(tokenInterceptor).addPathPatterns("/review-comments/create");
     registry.addInterceptor(tokenInterceptor).addPathPatterns("/reviews/**").excludePathPatterns("/reviews/find/**")
     .excludePathPatterns("/reviews/find-all*/**");
   }
