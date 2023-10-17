b84a080bfa5d773f1079cbf136fd8efc4a687a70
Victor Murilo <victor2706.vm@gmail.com>
2023-10-17 12:16:06 -0300

    feat: endpoint de criar um comentário em ReviewCommentsController

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsController.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsController.java
@@ -1,10 +1,25 @@
 package com.myhobbylistlmtd.springboot.reviewcomments;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myhobbylistlmtd.springboot.request.body.RequestCreateReviewComment;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/review-comments")
@@ -15,4 +30,40 @@ public class ReviewCommentsController {
    */
   @Autowired
   private ReviewCommentsService reviewCommentsService;
+
+  /**
+   * .
+   * @param userId
+   * @param body
+   * @return d
+   */
+  @PostMapping("/create")
+  @ResponseStatus(HttpStatus.CREATED)
+  @Operation(
+    summary = "Fazer um comentário em uma review",
+    parameters = {
+      @Parameter(
+        in = ParameterIn.HEADER,
+        schema = @Schema(implementation = String.class),
+        name = "Authorization",
+        required = true,
+        description = "JWT gerado ao fazer login ou registro"
+      )
+    }
+  )
+  @ApiResponses(value = {
+    @ApiResponse(
+      responseCode = "201",
+      description = "O comentário criada com sucesso",
+      content = { @Content(mediaType = "application/json",
+      schema = @Schema(implementation = ReviewComments.class)) }
+    )
+  })
+  ReviewComments createComment(
+    @RequestAttribute("userId") final Long userId,
+    @Valid @RequestBody final RequestCreateReviewComment body
+  ) {
+    ReviewComments test = this.reviewCommentsService.createReview(userId, body);
+    return test;
+  }
 }
