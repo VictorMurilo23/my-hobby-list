58d6c58b397aabd338e0597748b4d83c394e4a81
Victor Murilo <victor2706.vm@gmail.com>
2023-10-18 16:43:50 -0300

    feat: método de encontrar comentários de uma review em ReviewCommentsRepository

--- my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsRepository.java
+++ my-hobby-list-backend/src/main/java/com/myhobbylistlmtd/springboot/reviewcomments/ReviewCommentsRepository.java
@@ -1,6 +1,9 @@
 package com.myhobbylistlmtd.springboot.reviewcomments;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
@@ -10,4 +13,19 @@ public interface ReviewCommentsRepository extends JpaRepository<
   ReviewCommentsId
 > {
 
+  /**
+   * Encontra todos os comentários feitos em uma review.
+   * @param username Nome do usuário que fez a review
+   * @param mediaId Id da media
+   * @return Lista com todos os comentários
+   */
+  @Query(
+    value = "SELECT * FROM review_comments rc LEFT JOIN users us"
+    + " ON rc.review_id_user_id = us.id WHERE us.username = ?1"
+    + " AND rc.review_id_media_id = ?2 ORDER BY rc.insertion_date ASC",
+    nativeQuery = true
+  )
+  List<ReviewComments> findReviewsByUsernameAndMediaId(
+    String username, Long mediaId
+  );
 }
