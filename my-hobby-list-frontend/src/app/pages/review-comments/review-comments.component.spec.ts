3370940d24b60f616a7e9291d6ce45d835fc1c63
Victor Murilo <victor2706.vm@gmail.com>
2023-10-23 11:00:09 -0300

    feat: componente review-comments criado

---
+++ my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReviewCommentsComponent } from './review-comments.component';
+
+describe('ReviewCommentsComponent', () => {
+  let component: ReviewCommentsComponent;
+  let fixture: ComponentFixture<ReviewCommentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ReviewCommentsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ReviewCommentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-comments',
+  templateUrl: './review-comments.component.html',
+  styleUrls: ['./review-comments.component.css']
+})
+export class ReviewCommentsComponent {
+
+}
