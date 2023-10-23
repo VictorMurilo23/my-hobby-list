a32936f23a7ec1bedaf1b55d75632a648ec9d636
Victor Murilo <victor2706.vm@gmail.com>
2023-10-23 13:05:17 -0300

    feat: template com o input de criar um comentário

--- my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.ts
+++ my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { EMPTY, concatMap } from 'rxjs';
 import { Comment, Review, ReviewDetails } from 'src/app/interfaces/IReviews';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { ReviewService } from 'src/app/services/review.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-review-comments',
@@ -11,9 +13,11 @@ import { ReviewService } from 'src/app/services/review.service';
 })
 export class ReviewCommentsComponent implements OnInit {
   private review!: Review;
+  private createCommentInfo = { userComment: "", mediaId: 0, usernameFromReview: ""};
   private comments: Comment[] = [];
+  readonly tokenUsername: string | null = this.userService.getUsernameFromToken();
 
-  constructor(private reviewService: ReviewService, private activatedRoute: ActivatedRoute) {}
+  constructor(private reviewService: ReviewService, private activatedRoute: ActivatedRoute, private userService: UserService, private localStorage: LocalStorageService) {}
 
   ngOnInit(): void {
     this.activatedRoute.paramMap
@@ -22,6 +26,8 @@ export class ReviewCommentsComponent implements OnInit {
           const mediaId = params.get("mediaId");
           const username = params.get("username");
           if (mediaId !== null && username !== null) {
+            this.createCommentInfo.mediaId = Number(mediaId);
+            this.createCommentInfo.usernameFromReview = username;
             return this.reviewService.reviewDetails(username, mediaId);
           }
           return EMPTY;
@@ -44,5 +50,31 @@ export class ReviewCommentsComponent implements OnInit {
 
   public getComments() {
     return this.comments;
+  }
+
+  public getUserComment() {
+    return this.createCommentInfo.userComment;
+  }
+
+  public setUserComment(comment: string) {
+    this.createCommentInfo.userComment = comment;
+  }
+
+  public sendComment() {
+    const token = this.localStorage.getToken();
+    if (token === null) {
+      this.userService.logout();
+      return;
+    }
+    const { mediaId, userComment, usernameFromReview } = this.createCommentInfo;
+    this.reviewService.createComment(token, { commentary: userComment, mediaId, usernameFromReview })
+    .subscribe({
+      next: (data: Comment) => {
+        this.comments.push(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 }
