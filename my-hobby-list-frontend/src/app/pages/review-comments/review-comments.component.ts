b19570914ce54c4fef46ab8b1036fbb183c1cce2
Victor Murilo <victor2706.vm@gmail.com>
2023-10-23 11:30:07 -0300

    feat: review-comments.component conectando com o back end

--- my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.ts
+++ my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { EMPTY, concatMap } from 'rxjs';
+import { Comment, Review, ReviewDetails } from 'src/app/interfaces/IReviews';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-review-comments',
   templateUrl: './review-comments.component.html',
   styleUrls: ['./review-comments.component.css']
 })
-export class ReviewCommentsComponent {
+export class ReviewCommentsComponent implements OnInit {
+  private review!: Review;
+  private comments: Comment[] = [];
 
+  constructor(private reviewService: ReviewService, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap
+      .pipe(
+        concatMap((params: ParamMap) => {
+          const mediaId = params.get("mediaId");
+          const username = params.get("username");
+          if (mediaId !== null && username !== null) {
+            return this.reviewService.reviewDetails(username, mediaId);
+          }
+          return EMPTY;
+        })
+      )
+      .subscribe({
+        next: (data: ReviewDetails) => {
+          this.review = data.review;
+          this.comments.concat(data.comments);
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      });
+  }
 }
