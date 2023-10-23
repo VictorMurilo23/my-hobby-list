f06d508c1054febd899463d4a633c7e68fa8b94b
Victor Murilo <victor2706.vm@gmail.com>
2023-10-23 12:29:41 -0300

    test: testes de review-comments não falham mais

--- my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.spec.ts
+++ my-hobby-list-frontend/src/app/pages/review-comments/review-comments.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReviewCommentsComponent } from './review-comments.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import routes from 'src/app/app.routes';
+import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
 
 describe('ReviewCommentsComponent', () => {
   let component: ReviewCommentsComponent;
@@ -8,7 +12,8 @@ describe('ReviewCommentsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReviewCommentsComponent ]
+      imports: [HttpClientModule, RouterTestingModule.withRoutes(routes)],
+      declarations: [ ReviewCommentsComponent, PageNotFoundComponent ]
     })
     .compileComponents();
 
