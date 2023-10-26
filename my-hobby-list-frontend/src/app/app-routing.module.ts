66eecabb17398865a3363d8180b59d22764215ae
Victor Murilo <victor2706.vm@gmail.com>
2023-10-26 13:36:10 -0300

    feat: agora se algum parâmetro da rota mudar a página é recarregada

--- my-hobby-list-frontend/src/app/app-routing.module.ts
+++ my-hobby-list-frontend/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import routes from './app.routes';
-import { RouterModule } from '@angular/router';
+import { RouteReuseStrategy, RouterModule } from '@angular/router';
+import { CustomReuseStrategy } from './custom-reuse-estrategy';
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: "reload" })],
+  exports: [RouterModule],
+  providers: [
+    {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
+  ],
 })
 export class AppRoutingModule { }
