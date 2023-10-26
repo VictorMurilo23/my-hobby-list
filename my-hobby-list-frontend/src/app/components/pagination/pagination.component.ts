752eab26b369a3999eeefdbaac7d4da757940eb0
Victor Murilo <victor2706.vm@gmail.com>
2023-10-26 13:45:16 -0300

    feat: pagination.component renderizando a parte de mudar a página

--- my-hobby-list-frontend/src/app/components/pagination/pagination.component.ts
+++ my-hobby-list-frontend/src/app/components/pagination/pagination.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-pagination',
+  selector: 'app-pagination[totalPages][getContent][currentPage]',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
+  @Input() totalPages!: number;
+  @Input()
+  getContent!: (pageNumber: number) => void;
+  @Input() currentPage!: number
 
+  public getTotalPages() {
+    const arr = [];
+    for (let index = 1; index <= this.totalPages; index += 1) {
+      arr.push(index);
+    }
+    return arr;
+  }
+
+  public changePage(pageNum: number) {
+    this.currentPage = pageNum;
+    this.getContent(pageNum);
+  }
 }
