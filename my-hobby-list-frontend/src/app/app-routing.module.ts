adc82ab5db695d1e148360afc2e5a511d3ee5e82
Victor Murilo <victor2706.vm@gmail.com>
2023-10-30 10:44:23 -0300

    refactor: CustomReuseStrategy removido

--- my-hobby-list-frontend/src/app/app-routing.module.ts
+++ my-hobby-list-frontend/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import routes from './app.routes';
-import { RouteReuseStrategy, RouterModule } from '@angular/router';
-import { CustomReuseStrategy } from './custom-reuse-estrategy';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: "reload" })],
   exports: [RouterModule],
-  providers: [
-    {provide: RouteReuseStrategy, useClass: CustomReuseStrategy}
-  ],
+  providers: [],
 })
 export class AppRoutingModule { }
